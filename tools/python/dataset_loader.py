2bc7a89f855c8e9364ce99c1d242eaa8f6da75ba
Nicola Piga <nicolapiga@gmail.com>
2022-03-13 19:03:50 +0100

    Add python sample

---
+++ tools/python/dataset_loader.py
@@ -0,0 +1,105 @@
+#===============================================================================
+#
+# Copyright (C) 2022 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
+import cv2
+import json
+import numpy
+import os
+import struct
+
+
+class Loader():
+
+    def __init__(self, path, object_folder):
+        """Constructor."""
+
+        self._path = os.path.join(path, object_folder)
+        self._number_frames = None
+
+        self._load_number_frames()
+        self._load_camera_parameters()
+
+
+    def _load_number_frames(self):
+        """Load the total number of frames."""
+
+        self._number_frames = sum(1 for line in open(os.path.join(self._path, 'data.txt')))
+
+
+    def _load_camera_parameters(self):
+        """Load the camera parameters."""
+
+        self._camera_parameters = json.load(open(os.path.join(self._path, 'cam_K.json')))
+
+
+    def _is_valid_frame(self, number):
+        """Check if the frame with frame number 'number' does exists."""
+
+        return number < self._number_frames
+
+
+    def get_number_frames(self):
+        """Get the total number of frames."""
+
+        return self._number_frames
+
+
+    def get_camera_parameters(self):
+        """Get the camera parameterss."""
+
+        return self._camera_parameters
+
+
+    def get_rgb(self, number):
+        """Get the rgb frame given the frame number."""
+
+        if not self._is_valid_frame(number):
+            raise ValueError('The frame with frame number ' + str(number) + ' does not exist.')
+
+        return cv2.imread(os.path.join(self._path, 'rgb', str(number) + '.png'))
+
+
+    def get_depth(self, number):
+        """Get the depth frame given the frame number."""
+
+        if not self._is_valid_frame(number):
+            raise ValueError('The frame with frame number ' + str(number) + ' does not exist.')
+
+        with open(os.path.join(self._path, 'depth', str(number) + '.float'), 'rb') as f:
+            width, = struct.unpack('=Q', f.read(8))
+            height, = struct.unpack('=Q', f.read(8))
+            depth = numpy.reshape \
+                    (
+                        numpy.array(struct.unpack('f' * width * height, f.read())),
+                        (height, width)
+                    )
+
+        return depth
+
+
+    def get_optical_flow(self, number):
+        """Get the flow frame given the frame number."""
+
+        type_map = {cv2.CV_32FC2 : 'f', cv2.CV_16SC2 : 'h'}
+        type_scaling = {cv2.CV_32FC2 : 1.0, cv2.CV_16SC2 : float(2 ** 5)}
+
+        if not self._is_valid_frame(number):
+            raise ValueError('The frame with frame number ' + str(number) + ' does not exist.')
+
+        with open(os.path.join(self._path, 'optical_flow/nvof_1_slow', str(number) + '.float'), 'rb') as f:
+            frame_type, = struct.unpack('i', f. read(4))
+            width, = struct.unpack('=Q', f.read(8))
+            height, = struct.unpack('=Q', f.read(8))
+            flow = numpy.reshape \
+                (
+                    numpy.array(struct.unpack(type_map[frame_type] * width * height * 2, f.read())),
+                    (height, width, 2)
+                ) / type_scaling[frame_type]
+
+        return flow
---
+++ tools/python/sample.py
@@ -0,0 +1,52 @@
+#===============================================================================
+#
+# Copyright (C) 2022 Istituto Italiano di Tecnologia (IIT)
+#
+# This software may be modified and distributed under the terms of the
+# GPL-2+ license. See the accompanying LICENSE file for details.
+#
+#===============================================================================
+
+from dataset_loader import Loader
+import cv2
+import imgviz
+import numpy
+
+
+def main():
+    fastycb_path = 'path_to_fastycb'
+    object_folder = '003_cracker_box'
+    loader = Loader(path = fastycb_path, object_folder = object_folder)
+
+    # Print useful information
+    print('# frames: ', loader.get_number_frames())
+    print('camera parameters')
+    print(loader.get_camera_parameters())
+
+    # Show sample RGB, depth and optical flow frames
+    for i in range(500, min(1000, loader.get_number_frames())):
+        # Get all frames
+        rgb = loader.get_rgb(i)
+        depth = loader.get_depth(i)
+        flow = loader.get_optical_flow(i)
+
+        # Render depth and optical flow using RGB colors
+        depth_render = imgviz.depth2rgb(depth, min_value = 0.3, max_value = 1.5, colormap = 'rainbow')
+        flow_render = imgviz.flow2rgb(flow)
+
+        # Render RGB, depth and optical flow in a single frame
+        scale = 0.4
+        height = int(rgb.shape[0] * scale)
+        width = int(rgb.shape[1] * scale)
+        render = numpy.empty([height, 3 * width, 3], 'uint8')
+
+        render[:, : width] = cv2.resize(rgb, (width, height))
+        render[:, width : 2 * width] = cv2.resize(depth_render, (width, height))
+        render[:, 2 * width : 3 * width] = cv2.resize(flow_render, (width, height))
+
+        cv2.imshow('', render)
+        cv2.waitKey(33)
+
+
+if __name__ == '__main__':
+    main()
