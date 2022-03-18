369e1d7549141506c33dca6326e039503d52f335
Nicola Piga <nicolapiga@gmail.com>
2022-03-18 14:50:11 +0000

    Add message about currently downloaded file in script for dataset download

--- tools/download/download_dataset.sh
+++ tools/download/download_dataset.sh
@@ -9,6 +9,7 @@ curl -H X-Dataverse-key:$TOKEN $SERVER/api/datasets/:persistentId/versions/$VERS
 # Download all files in the dataset
 NUM_FILES=`cat dataset.json | jq ".data | length - 1"`
 for i in $(seq 0 $NUM_FILES); do
+    echo "Downloading file $i/$NUM_FILES..."
     file_id=`cat dataset.json | jq ".data[$i].dataFile.id"`
     file_name=`cat dataset.json | jq ".data[$i].dataFile.filename" | tr -d '"'`
     curl -L -H X-Dataverse-key:$TOKEN $SERVER/api/access/datafile/$file_id -o $file_name
